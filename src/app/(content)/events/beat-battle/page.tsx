479fc4ebbfbbc56d6fca7f5ad8827328612ee1f2
aryan2427 <aryan792005@gmail.com>
2026-01-20 00:19:22 +0530

    beatbattlecardupdate

--- src/app/(content)/events/beat-battle/page.tsx
+++ src/app/(content)/events/beat-battle/page.tsx
@@ -64,7 +64,7 @@ const Page = () => {
       <EventIntro
         venue="Babuji Chowk, OPJU"
         time="20 Feb 2026, 2:00 PM - 5:30 PM"
-        imageUrl="/testfile/BeatBattle.png"
+        imageUrl="/testfile/beat.svg"
         registrations={registrationCount}
         pricepool={10000} // Can update prize if known
         description="Step into a high-energy clash of rhythm, creativity, and raw talent at Beat Battle. Dancers face off in head-to-head rounds, trading explosive footwork, sharp musicality, and fearless improvisation. The crowd fuels the momentum, judges watch for originality and control, and every beat drop is a chance to shine."
