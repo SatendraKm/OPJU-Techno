f018b47608ccb4bc591888baec78f92f7991cf5f
aryan2427 <aryan792005@gmail.com>
2026-01-20 12:38:08 +0530

    leftoverpagebg

--- src/app/(content)/events/techlab/page.tsx
+++ src/app/(content)/events/techlab/page.tsx
@@ -67,7 +67,7 @@ const Page = () => {
         <Image
           width={500}
           height={500}
-          src="/background.svg"
+          src="/testfile/singleeventbg.svg"
           className="w-full h-auto opacity-100"
           alt="Scrolling Background"
         />
@@ -76,7 +76,7 @@ const Page = () => {
       {/* Event Intro Section */}
       <section>
         <EventIntro
-          imageUrl="/testfile/techlab.svg"
+          imageUrl="/testfile/techlabnew.svg"
           registrations={registrationCount}
           pricepool={50000}
           description="Tech lab is the flagship event of the biggest tech festival in central India, TECHNOROLLIX. It’s a vibrant showcase of innovation and ingenuity, where the brightest minds from various universities unleash their creative potential through their self-developed models. Creating a technical model presentation involves effectively communicating the details, functionality, and significance of a technical model to a diverse audience, which may include technical and non-technical stakeholders. This platform also opens doors to better funding opportunities for these innovative minds, paving the way to a future breakthrough"
--- src/app/(content)/events/yuva-sabha/page.tsx
+++ src/app/(content)/events/yuva-sabha/page.tsx
@@ -94,7 +94,7 @@ const Page = () => {
       {/* Background Image */}
       <div className="absolute top-0 left-0 w-full h-full pointer-events-none -z-10">
         <Image
-          src="/background.svg"
+          src="/testfile/singleeventbg.svg"
           alt="Scrolling Background"
           width={500}
           height={500}
