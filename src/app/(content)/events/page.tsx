b310074382009445b4d4bcd46c0f879bfd00df2e
aryan2427 <aryan792005@gmail.com>
2026-01-23 22:57:06 +0530

    event page bg  and cards change

--- src/app/(content)/events/page.tsx
+++ src/app/(content)/events/page.tsx
@@ -4,24 +4,24 @@ import Link from "next/link";
 
 const eventsData = {
   technical: [
-    { imageUrl: "/testfile/codigo.svg", link: "/events/codigo" },
-    { imageUrl: "/testfile/techlabnew.svg", link: "/events/techlab" },
-    { imageUrl: "/testfile/robo.svg", link: "/events/robovation" },
+    { imageUrl: "/testfile/codigo3.svg", link: "/events/codigo" },
+    { imageUrl: "/testfile/techlab3.svg", link: "/events/techlab" },
+    { imageUrl: "/testfile/robovation3.svg", link: "/events/robovation" },
     { imageUrl: "/testfile/ideathon.svg", link: "/events/ideathon" },
     { imageUrl: "/testfile/backtrace3.svg", link: "/events/reverseEng" },
-    { imageUrl: "/testfile/design.svg", link: "/events/designathon" },
+    { imageUrl: "/testfile/Designathon3.svg", link: "/events/designathon" },
   ],
 
   nonTechnical: [
-    { imageUrl: "/testfile/kalakritinew.svg", link: "/events/kalakriti" },
-    { imageUrl: "/testfile/master.svg", link: "/events/master_chef" },
-    { imageUrl: "/testfile/roadies.svg", link: "/events/roadies" },
-    { imageUrl: "/testfile/antaragini.svg", link: "/events/antaragni" },
-    { imageUrl: "/testfile/voice.svg", link: "/events/yuva-sabha" },
-    { imageUrl: "/testfile/beat.svg", link: "/events/beat-battle" },
-    { imageUrl: "/testfile/startup.svg", link: "/events/start-up-business-plan" },
+    { imageUrl: "/testfile/kalakriti3.svg", link: "/events/kalakriti" },
+    { imageUrl: "/testfile/masterchef3.svg", link: "/events/master_chef" },
+    { imageUrl: "/testfile/rodies3.svg", link: "/events/roadies" },
+    { imageUrl: "/testfile/Antragini3.svg", link: "/events/antaragni" },
+    { imageUrl: "/testfile/voiceofyouth.svg", link: "/events/yuva-sabha" },
+    { imageUrl: "/testfile/beatbattle.svg", link: "/events/beat-battle" },
+    { imageUrl: "/testfile/startup3.svg", link: "/events/start-up-business-plan" },
     { imageUrl: "/testfile/riwayat4.svg", link: "/events/riywayat" },
-    { imageUrl: "/testfile/aima3.svg", link: "/events/AIMA" },
+    { imageUrl: "/testfile/aima3.svg", link: "/events/aima" },
   ],
 };
 
@@ -37,7 +37,7 @@ const EventSection: React.FC<EventSectionProps> = ({ title, events }) => (
       {title}
     </h2>
 
-    <p className="mt-4 md:mt-10 max-w-[943px] text-center text-black text-base md:text-xl font-medium font-['Poppins'] uppercase mx-auto px-2">
+    <p className="mt-4 md:mt-10 max-w-[943px] text-center text-white text-base md:text-xl font-medium font-['Poppins'] uppercase mx-auto px-2">
       Our fest offers a diverse range of technical and non-technical events,
       ensuring there&apos;s something for everyone. From innovative tech
       showcases to fun and engaging activities, we bring together creativity,
@@ -67,7 +67,7 @@ const EventSection: React.FC<EventSectionProps> = ({ title, events }) => (
     <div className="flex justify-center mt-12">
       <Link href="/dashboard">
         <Image
-          src="/testfile/register1.svg"
+          src="/testfile/register2.svg"
           alt="Register Button"
           width={260}
           height={70}
@@ -84,7 +84,7 @@ const Page = () => {
   return (
    <div
   className="w-full min-h-screen pt-24 md:pt-28 bg-no-repeat bg-top bg-cover"
-  style={{ backgroundImage: "url('/testfile/newevent.png')" }}
+  style={{ backgroundImage: "url('/testfile/events (3).svg')" }}
 >
 
       <div className="w-full">
