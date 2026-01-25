136db32e003dcad7a4d98906c571e434f67541fd
Prince Raj <63579641+princeraj2572@users.noreply.github.com>
2026-01-25 12:33:59 +0530

    registration counter still problem need to be fixed , changed the kalakriti content , added the new sub event and removed old one , instagram icon link in footer

--- src/data/event-subeventData.js
+++ src/data/event-subeventData.js
@@ -1,40 +1,31 @@
 export const eventSubEventData = {
   events: [
     {
-      eventName: "TECHLAB",
-      event: "",
-      subEvents: [//696e7207cd8b052c9846b6bd
-        { subEventName: "Working", subEvent: "696e7207cd8b052c9846b6bd" },
-        { subEventName: "Non-working", subEvent: "696e7207cd8b052c9846b6be" },
-        {
-          subEventName: "Poster Presentation",
-          subEvent: "67c15d4de3c4c72ebbd459d5",
-        },
-        { subEventName: "App Making", subEvent: "696e7207cd8b052c9846b6c0" },
-      ],
+      eventName: "BACKTRACE",
+      event: "696e7207cd8b052c9846b6cb",
+      subEvents: [],
     },
     {
-      eventName: "ANTARAGNI",
+      eventName: "KALAKRITI",
       event: "",
       subEvents: [
-        { subEventName: "Singing", subEvent: "696e7207cd8b052c9846b6df" },
-        { subEventName: "Rap", subEvent: "696e7207cd8b052c9846b6e0" },
-        { subEventName: "Short Film", subEvent: "696e7207cd8b052c9846b6e2" },
-        { subEventName: "Solo Dance", subEvent: "696e7207cd8b052c9846b6dd" },
-        { subEventName: "Group Dance", subEvent: "696e7207cd8b052c9846b6de" },
+        { subEventName: "Darbar-e-Bharat (Decoration)", subEvent: "696e7207cd8b052c9846b6d4" },
+        { subEventName: "Alpana", subEvent: "696e7207cd8b052c9846b6d8" },
+        { subEventName: "Rangoli", subEvent: "696e7207cd8b052c9846b6d7" },
+        { subEventName: "Colour Painting", subEvent: "696e7207cd8b052c9846b6d6" },
+        { subEventName: "Faces That Speak", subEvent: "696e7207cd8b052c9846b6d9" },
+        { subEventName: "Waste to Wow", subEvent: "696e7207cd8b052c9846b6d5" },
+        { subEventName: "Pradarshini (Art Exhibition)", subEvent: "696e7207cd8b052c9846b6d3" },
       ],
     },
     {
       eventName: "ROBOVATION",
       event: "",
       subEvents: [
-        { subEventName: "Robo War", subEvent: "696e7207cd8b052c9846b6c6" },
-        { subEventName: "Robo Soccer", subEvent: "696e7207cd8b052c9846b6c3" },
         { subEventName: "Robo Race", subEvent: "696e7207cd8b052c9846b6c4" },
-        {
-          subEventName: "Fast Line Follower",
-          subEvent: "67c1623ee3c4c72ebbd45a23",
-        },
+        { subEventName: "Robo Soccer", subEvent: "696e7207cd8b052c9846b6c3" },
+        { subEventName: "Fast Line Follower", subEvent: "696e7207cd8b052c9846b6c5" },
+        { subEventName: "Robo War", subEvent: "696e7207cd8b052c9846b6c6" },
       ],
     },
     {
@@ -43,92 +34,76 @@ export const eventSubEventData = {
       subEvents: [],
     },
     {
-      eventName: "KALAKRITI",
+      eventName: "ANTARAGNI",
       event: "",
       subEvents: [
-        { subEventName: "Pradarshini", subEvent: "696e7207cd8b052c9846b6d3" },
-        {
-          subEventName: "wow to wow ",
-          subEvent: "696e7207cd8b052c9846b6d5",
-        },
-        { subEventName: "Faces That Speak", subEvent: "696e7207cd8b052c9846b6d9" },
-        {
-          subEventName: "Rangoli",
-          subEvent: "696e7207cd8b052c9846b6d7",
-        },
-        { subEventName: "Colour Painting", subEvent: "696e7207cd8b052c9846b6d6" },
-        { subEventName: "Alpana", subEvent: "696e7207cd8b052c9846b6d8" },
+        { subEventName: "Mime", subEvent: "696e7207cd8b052c9846b6e1" },
+        { subEventName: "Group Dance", subEvent: "696e7207cd8b052c9846b6de" },
+        { subEventName: "Short Film", subEvent: "696e7207cd8b052c9846b6e2" },
+        { subEventName: "Solo Dance", subEvent: "696e7207cd8b052c9846b6dd" },
+        { subEventName: "Rap / Beatboxing", subEvent: "696e7207cd8b052c9846b6e0" },
+        { subEventName: "Singing", subEvent: "696e7207cd8b052c9846b6df" },
       ],
     },
-   
-   {
-      eventName: "CODIGO",
-      event: "",
-      subEvents: [
-        { subEventName: "Codigo  Design Master (UI/UX)", subEvent: "696e7207cd8b052c9846b6c8" },
-        { subEventName: "Codigo  Coding Challenge", subEvent: "696e7207cd8b052c9846b6c7" },
-      ],
-    },
-   
-    
-   {
+    {
       eventName: "IDEATHON",
       event: "",
       subEvents: [
-        { subEventName: "Ideathon 1", subEvent: "696e7207cd8b052c9846b6c1" },
-        { subEventName: "Ideathon 2", subEvent: "696e7207cd8b052c9846b6c2" },
+        { subEventName: "SDG Challenge", subEvent: "696e7207cd8b052c9846b6c2" },
+        { subEventName: "Ideation & Presentation", subEvent: "696e7207cd8b052c9846b6c1" },
       ],
     },
-    {
-      eventName: "RIWAYAT-(FASHION-SHOW)",
-      event: "696e7207cd8b052c9846b6da",
-      subEvents: [],
-    },//696e7207cd8b052c9846b6da
-    {
-      eventName: "ROADIES",
-      event: "696e7207cd8b052c9846b6d0",
-      subEvents: [],
-      //696e7207cd8b052c9846b6d0
-    },
-    {
-      eventName: "DESIGNATHON",
-      event: "696e7207cd8b052c9846b6ca",
-      subEvents: [],
-      //696e7207cd8b052c9846b6c9
-      //696e7207cd8b052c9846b6ca
-      //696e7207cd8b052c9846b6ca
-    },
-    {
-      eventName: "AIMA",
-      event: "696e7207cd8b052c9846b6d1",
-      subEvents: [],
-    },//696e7207cd8b052c9846b6d1
-    //696e7207cd8b052c9846b6d1
-    {
-      eventName: "BACKTRACE",
-      event: "696e7207cd8b052c9846b6cb",
-      subEvents: [],
-      //696e7207cd8b052c9846b6cb
-      //696e7207cd8b052c9846b6cb
-    },
-   //696e7207cd8b052c9846b6cb
-   //696e7207cd8b052c9846b6cb
-    {
-      eventName: "VOICE-OF-YOUTH",
-      event: "696e7207cd8b052c9846b6ce",
-      //696e7207cd8b052c9846b6ce
-      subEvents: [],
-    },
-  
-    
     {
       eventName: "BEAT-BATTLE",
       event: "696e7207cd8b052c9846b6d2",
       subEvents: [],
     },
     {
+      eventName: "TECHLAB",
+      event: "",
+      subEvents: [
+        { subEventName: "Working Models", subEvent: "696e7207cd8b052c9846b6bd" },
+        { subEventName: "Non-Working Models", subEvent: "696e7207cd8b052c9846b6be" },
+        { subEventName: "App Making", subEvent: "696e7207cd8b052c9846b6c0" },
+        { subEventName: "Poster Presentation", subEvent: "696e7207cd8b052c9846b6bf" },
+      ],
+    },
+    {
       eventName: "STARTUP-BUSINESS-PLAN",
       event: "696e7207cd8b052c9846b6ce",
+      subEvents: [],
+    },
+    {
+      eventName: "RIWAYAT-(FASHION-SHOW)",
+      event: "696e7207cd8b052c9846b6da",
+      subEvents: [],
+    },
+    {
+      eventName: "ROADIES",
+      event: "696e7207cd8b052c9846b6d0",
+      subEvents: [],
+    },
+    {
+      eventName: "VOICE-OF-YOUTH",
+      event: "696e7207cd8b052c9846b6cf",
+      subEvents: [],
+    },
+    {
+      eventName: "AIMA",
+      event: "696e7207cd8b052c9846b6d1",
+      subEvents: [],
+    },
+    {
+      eventName: "CODIGO",
+      event: "",
+      subEvents: [
+        { subEventName: "Design Master (UI/UX)", subEvent: "696e7207cd8b052c9846b6c8" },
+        { subEventName: "Coding Challenge", subEvent: "696e7207cd8b052c9846b6c7" },
+      ],
+    },
+    {
+      eventName: "DESIGNATHON",
+      event: "696e7207cd8b052c9846b6ca",
       subEvents: [],
     },
   ],
