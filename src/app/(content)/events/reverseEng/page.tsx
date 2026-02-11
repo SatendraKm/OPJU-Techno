cceaaad7b3f830c90b7da964e61659af4cd6cc33
surya prakash <sharmasuryaprakash1234@gmail.com>
2026-02-11 22:50:30 +0530

    backtrace timing fixed

--- src/app/(content)/events/reverseEng/page.tsx
+++ src/app/(content)/events/reverseEng/page.tsx
@@ -87,8 +87,8 @@ const Page = () => {
         registrations={registrationCount}
         pricepool={11000}
         description="Reverse Engineering – Decode the Hardware is a logic-oriented technical event designed to evaluate participants’ ability to analyze, interpret, and reason about unknown hardware systems. The event emphasizes observation, deduction, and conceptual understanding rather than physical assembly or complex instrumentation."
-        time="19, 20 & 21 Feb | 1:00 PM – 5:30 PM"
-        venue="TB 01 & TB 07"
+        time="19 & 20 Feb | 1:00 PM – 5:30 PM"
+        venue="TB 01"
       />
       {/* </a> */}
 
